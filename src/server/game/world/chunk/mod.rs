6cf8dd568e04372c03f59bdcd3da52bc9c980f9e
DamyanS <damyanslavov@gmail.com>
2023-10-06 00:48:50 +0200

    non_air_count

--- src/server/game/world/chunk/mod.rs
+++ src/server/game/world/chunk/mod.rs
@@ -19,30 +19,30 @@ use std::{
 #[derive(Default)]
 pub struct Chunk {
     blocks: [[[Block; Self::DIM]; Self::DIM]; Self::DIM],
-    count: usize,
+    non_air_count: usize,
 }
 
 impl Chunk {
     pub const DIM: usize = 16;
 
     fn from_fn<F: FnMut(Point3<u8>) -> Block>(mut f: F) -> Self {
-        let mut count = 0;
+        let mut non_air_count = 0;
         Self {
             blocks: array::from_fn(|x| {
                 array::from_fn(|y| {
                     array::from_fn(|z| {
                         let block = f(point![x, y, z].cast());
-                        count += (block != Block::Air) as usize;
+                        non_air_count += (block != Block::Air) as usize;
                         block
                     })
                 })
             }),
-            count,
+            non_air_count,
         }
     }
 
     pub fn is_empty(&self) -> bool {
-        self.count == 0
+        self.non_air_count == 0
     }
 
     pub fn apply(&mut self, coords: Point3<u8>, action: &BlockAction) -> bool {
@@ -51,12 +51,12 @@ impl Chunk {
             BlockAction::Place(Block::Air) => false,
             BlockAction::Place(curr) if *prev == Block::Air => {
                 *prev = *curr;
-                self.count += 1;
+                self.non_air_count += 1;
                 true
             }
             BlockAction::Destroy if *prev != Block::Air => {
                 *prev = Block::Air;
-                self.count -= 1;
+                self.non_air_count -= 1;
                 true
             }
             _ => false,
@@ -68,8 +68,8 @@ impl Chunk {
         let curr = action.placed_block();
         match (mem::replace(prev, curr), curr) {
             (Block::Air, Block::Air) => {}
-            (Block::Air, _) => self.count += 1,
-            (_, Block::Air) => self.count -= 1,
+            (Block::Air, _) => self.non_air_count += 1,
+            (_, Block::Air) => self.non_air_count -= 1,
             _ => {}
         }
     }
