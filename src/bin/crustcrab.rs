31ec395b016a5e8ed7e081a911d9b3f60d1d7330
Damyan Slavov <damyanslavov@gmail.com>
2025-02-17 05:05:27 +0100

    into you

--- src/bin/crustcrab.rs
+++ src/bin/crustcrab.rs
@@ -7,8 +7,8 @@ use std::thread;
 fn main() {
     let (client_tx, client_rx) = crossbeam_channel::unbounded();
     let client = Client::new(client_tx);
-    let server_tx = ServerSender::Proxy(client.create_proxy());
-    let mut server = Server::new(server_tx, client_rx);
+    let proxy = client.create_proxy();
+    let mut server = Server::new(ServerSender::Proxy(proxy), client_rx);
     thread::spawn(move || server.run());
     client.run();
 }
